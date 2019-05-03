b32537bb0da3d48380dd77b68711ff84a445491b
miiicom <miiicom1230@gmail.com>
2019-05-03 16:04:19 -0600

    update

---
+++ C++ScratchPas/TripleX.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main() {	
+	int currentLevel = 2;
+	int maxLevel = 10;
+
+	std::cout << "Solve this" << std::endl;
+	std::cout << "enter the correct code to continue" << std::endl;
+	std::cout << maxLevel;
+	return 0;
+}
