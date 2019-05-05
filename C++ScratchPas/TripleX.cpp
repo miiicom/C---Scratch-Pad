8f707c5925efe66a44ff35fe5573e4b99cad405f
miiicom <miiicom1230@gmail.com>
2019-05-04 19:55:00 -0600

    update

--- C++ScratchPas/TripleX.cpp
+++ C++ScratchPas/TripleX.cpp
@@ -2,11 +2,16 @@
 
 
 int main() {	
+	//Declare variables
 	int currentLevel = 2;
-	int maxLevel = 10;
+	const int maxLevel = 10; // tell compiler never allow this value to be chnanged
+
+	//print UI informations
 
 	std::cout << "Solve this" << std::endl;
 	std::cout << "enter the correct code to continue" << std::endl;
-	std::cout << maxLevel;
+
+	std::cout << "Your current level add maxlevel is" << maxLevel + currentLevel << std::endl;
+	std::cout << "currentLevel multiply by  PAI is" << currentLevel * 3.1415927 << std::endl;
 	return 0;
 }
