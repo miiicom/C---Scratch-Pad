8ddd554bfe9f70debf6529e2c43439eb6f5c94ab
miiicom <miiicom1230@gmail.com>
2019-05-06 15:21:41 -0600

    update

--- C++ScratchPas/TripleX.cpp
+++ C++ScratchPas/TripleX.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
-void printIntroduction() {
-	std::cout << "Solve this" << std::endl;
+#include <ctime>
+void printIntroduction( int difficulty) {
+	std::cout << "Solve this in difficulty : " << difficulty<< std::endl;
 	std::cout << "enter the correct code to continue" << std::endl;
 }
 
 
-bool playgame() {
-	printIntroduction();
+bool playgame(int diffuculty) {
+	printIntroduction(diffuculty);
 	//Declare variables
 	
 	int playerInput1, playerInput2, playerInput3;
 	int playerGuessSum, playerGuessProduct;
 	int numSum, numProduct;
 
-	int num1 = 3;
-	int num2 = 4;
-	int num3 = 5;
+	int num1 = rand() % diffuculty + diffuculty;
+	int num2 = rand() % diffuculty + diffuculty;
+	int num3 = rand() % diffuculty + diffuculty;
 
 	numSum = num1 + num2 + num3;
 	numProduct = num1 * num2 *num3;
@@ -28,8 +29,6 @@ bool playgame() {
 	std::cin >> playerInput1;
 	std::cin >> playerInput2;
 	std::cin >> playerInput3;
-	std::cin.clear();
-	std::cin.ignore();
 	
 
 	std::cout << "your input is " << playerInput1 << " and " << playerInput2 << " and " << playerInput3 << std::endl;
@@ -41,7 +40,7 @@ bool playgame() {
 		return true;
 	}
 	else {
-		std::cout << "You died \n";
+		std::cout << "You died \n Go retry the game:) \n";
 		return false;
 	}
 
@@ -50,19 +49,21 @@ bool playgame() {
 
 
 int main() {	
-	int currentLevel = 2;
+	srand(time(NULL));
+	int currentLevel = 1;
 	const int maxLevel = 10; // tell compiler never allow this value to be chnanged
 	while (currentLevel <= maxLevel) {
 		
-		if (playgame()) {
+		if (playgame(currentLevel)) {
 			std::cin.clear();
 			std::cin.ignore();
 			currentLevel += 1;
 			std::cout << "next level! XDDDD \n";
 		}else{
-			return 0;
+			
 		}
 	}
+	std::cout << "You have completed the game!\n";
 
 	return 0;
 }
